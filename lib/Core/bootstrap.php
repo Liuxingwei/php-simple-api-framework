76c4b0aae0343758080e496c7895607624ee6169
liuxingwei <matchless@163.com>
2019-09-25 21:28:44 +0800

    在抛出非HTTP CODE错误时，将错误码转换为500。

--- lib/Core/bootstrap.php
+++ lib/Core/bootstrap.php
@@ -28,5 +28,52 @@ set_exception_handler(function (Throwable $ex) {
             ],
         ],
     ];
-    \Lib\Core\Response::json($response, $ex->getCode());
+
+    $httpCodes = [
+        "100",
+        "101",
+        "200",
+        "202",
+        "203",
+        "204",
+        "205",
+        "206",
+        "300",
+        "301",
+        "302",
+        "303",
+        "304",
+        "305",
+        "307",
+        "400",
+        "401",
+        "402",
+        "403",
+        "404",
+        "405",
+        "406",
+        "407",
+        "408",
+        "409",
+        "410",
+        "411",
+        "412",
+        "413",
+        "414",
+        "415",
+        "416",
+        "417",
+        "500",
+        "501",
+        "502",
+        "503",
+        "504",
+        "505"
+    ];
+    if (in_array($ex->getCode(), $httpCodes)) {
+        $httpCode = $ex->getCode();
+    } else {
+        $httpCode = 500;
+    }
+    \Lib\Core\Response::json($response, $httpCode);
 });
