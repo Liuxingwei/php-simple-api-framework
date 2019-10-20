febc6448e575335ef93db77a87209f56d9e6c26e
liuxingwei <matchless@163.com>
2019-10-20 22:54:05 +0800

    加入开发有实验类

--- application/Api/Get/Test/Index.php
+++ application/Api/Get/Test/Index.php
@@ -2,18 +2,71 @@
 
 namespace Application\Api\Get\Test;
 
-use Lib\Core\AbstractBaseApi;
+use Lib\Core\BaseApiInterface;
 use Lib\Core\DB;
 use Lib\Core\ErrorCode;
+use Application\Validation;
 
-class Index extends AbstractBaseApi
+class Index implements BaseApiInterface
 {
-    public function run()
+    /**
+     * 默认数据库
+     *
+     * @var DB
+     */
+    private $db;
+
+    /**
+     * 第二个数据库
+     *
+     * @var DB
+     */
+    private $secondDb;
+
+    /**
+     * 设置数据库
+     *
+     * @Inject
+     * @param DB $db
+     * @return void
+     */
+    public function setDb(DB $db)
     {
-        $db = DB::getInstance('t_user');
-        $res = $db->where('user_id = 10000')
+        $this->db = $db;
+    }
+
+    /**
+     * 设置第二个数据库
+     *
+     * @Inject({"second_db"})
+     * @param DB $db
+     * @return void
+     */
+    public function setSecondDb($db)
+    {
+        $this->secondDb = $db;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @Required({"abc", "def"})
+     * @param array $request
+     * @return void
+     */
+    public function run(array $request)
+    {
+        $this->db->table('t_user');
+        $res = $this->db->where('user_id = 1')
+            ->selectOne();
+        $this->secondDb->table('users');
+        $user = $this->secondDb->where('user_id = :user_id', [':user_id' => 'admin'])
             ->selectOne();
         $result = ErrorCode::OK;
-        $this->responseJson($result);
+        $result['data'] = [
+            'user_info' => $res,
+            'user' => $user,
+        ];
+        return $result;
     }
 }
