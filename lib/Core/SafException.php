f2573a87e817d4376a064eaa333dd860ac600334
David Liu <David.liu@sinoeyes.com>
2019-10-22 09:11:08 +0800

    为异常类添加注释

--- lib/Core/SafException.php
+++ lib/Core/SafException.php
@@ -1,12 +1,22 @@
 <?php
+
 namespace Lib\Core;
 
 use Exception;
 
+/**
+ * 自定义异常类，用于抛出符合json格式，可用于框架输出的消息
+ */
 class SafException extends Exception
 {
-    public static function throw($errorCode) {
+    /**
+     * 抛出框架定义的错误
+     *
+     * @param array $errorCode 需要包含 code 和 message 元素。
+     * @return void
+     */
+    public static function throw($errorCode)
+    {
         throw new SafException($errorCode['message'], $errorCode['code']);
     }
 }
-
