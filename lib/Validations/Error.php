3c71f121b812902d4efee11f50359a52603c6d33
David Liu <David.liu@sinoeyes.com>
2019-10-22 16:30:48 +0800

    对 Lib\Validations\Error 的属性进行封装。

--- lib/Validations/Error.php
+++ lib/Validations/Error.php
@@ -2,6 +2,8 @@
 
 namespace Lib\Validations;
 
+use InvalidArgumentException;
+
 /**
  * 校验错误实体
  */
@@ -12,12 +14,44 @@ class Error
      *
      * @var int
      */
-    public $code = null;
+    private $code = null;
 
     /**
      * 错误消息
      *
      * @var string
      */
-    public $message = null;
+    private $message = null;
+
+    /**
+     * 为错误编码和错误消息赋值
+     *
+     * @param string $name
+     * @param int|string $value
+     * @throws InvalidArgumentException
+     */
+    public function __set($name, $value)
+    {
+        if (property_exists(__CLASS__, $name)) {
+            $this->$name = $value;
+        } else {
+            throw InvalidArgumentException('Property "' . $name . '" not exists.');
+        }
+    }
+
+    /**
+     * 获取错误编码和错误消息
+     *
+     * @param string $name
+     * @return int|string
+     * @throws InvalidArgumentException
+     */
+    public function __get($name)
+    {
+        if (property_exists(__CLASS__, $name)) {
+            return $this->$name;
+        } else {
+            throw InvalidArgumentException('Property "' . $name . '" not exsists.');
+        }
+    }
 }
