79771b970225d438d64f1fe93c9718c1d97b07b5
David Liu <David.liu@sinoeyes.com>
2019-11-05 13:43:03 +0800

    修改启动入口的跨域代码。

--- lib/Core/bootstrap.php
+++ lib/Core/bootstrap.php
@@ -2,20 +2,25 @@
 defined('ROOT') || define('ROOT', dirname(dirname(__DIR__)));
 require_once ROOT . '/vendor/autoload.php';
 $configFile = ROOT . '/conf/config.php';
-if (file_exists($configFile)) {
-    $_ENV['config'] = include $configFile;
-} else {
-    $_ENV['config'] = [];
-}
+$_ENV['config'] = file_exists($configFile) ? include $configFile : [];
 defined('CONFIG') || define('CONFIG', $_ENV['config']);
 defined('DB_CONFIG') || (isset(CONFIG['db']) && define('DB_CONFIG', CONFIG['db']));
 defined('MODEL_NAMESPACE') || (isset(CONFIG['model_namespace']) && define('MODEL_NAMESPACE', CONFIG['model_namespace']));
-if (isset($_ENV['config']['runtime']) && $_ENV['config']['runtime'] != 'product') {
-    header("Access-Control-Allow-Origin: http://localhost:8080");
-    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
-    header('Access-Control-Allow-Headers: x-requested-with, content-type, key, debug');
-    header('Access-Control-Allow-Credentials: true');
-}
+crossDomain: (function () {
+    if (isset($_ENV['config']['runtime']) && $_ENV['config']['runtime'] != 'product') {
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $url = parse_url($_SERVER['HTTP_REFERER']);
+            $href = $url['scheme'] . '://' . $url['host'];
+            isset($url['port']) && $href .= ':' . $url['port'];
+        } else {
+            $href = '*';
+        }
+        header("Access-Control-Allow-Origin: $href");
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+        header('Access-Control-Allow-Headers: x-requested-with, content-type, key, debug');
+        header('Access-Control-Allow-Credentials: true');
+    }
+})();
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     throw new ErrorException($errstr, 500, $errno, $errfile, $errline);
 });
